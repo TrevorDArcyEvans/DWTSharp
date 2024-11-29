20571234f68f55b45f545400c7dd291f33de38a4
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-11-29 17:12:00 +0000

    - calculate match

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DWTSharp;
+﻿using System.Numerics;
+
+namespace DWTSharp;
 
 using CommandLine;
 using SixLabors.ImageSharp;
@@ -17,10 +19,46 @@ internal static class Program
   {
     var img1 = await Image.LoadAsync<Rgba32>(opt.ImageFile1Path);
     var img2 = await Image.LoadAsync<Rgba32>(opt.ImageFile2Path);
-    var match = 0d;
+    var data1 = ImageDecomposition.GetGrayscaleDate(img1, opt.HashSize);
+    var data2 = ImageDecomposition.GetGrayscaleDate(img2, opt.HashSize);
+    DWTHaar.FWT(data1, 1);
+    DWTHaar.FWT(data2, 1);
+
+    var match = (1d - Distance(data1, data2)) * 100d;
     Console.WriteLine($"ImageFile1 = {opt.ImageFile1Path}");
     Console.WriteLine($"ImageFile2 = {opt.ImageFile2Path}");
     Console.WriteLine($"  Match = {match:F1}%");
+  }
+
+  private static double Average<T>(T[,] data) where T : INumber<T>
+  {
+    var sum = 0d;
+    for (var i = 0; i < data.GetLength(0); i++)
+    {
+      for (var j = 0; j < data.GetLength(1); j++)
+      {
+        sum += double.CreateChecked(data[i, j]);
+      }
+    }
+
+    return sum / (data.GetLength(0) * data.GetLength(1));
+  }
+
+  private static double Distance<T>(T[,] data1, T[,] data2) where T : INumber<T>
+  {
+    var avg1 = Average(data1);
+    var avg2 = Average(data2);
+
+    var sumDistSq = 0d;
+    for (var i = 0; i < data1.GetLength(0); i++)
+    {
+      for (var j = 0; j < data1.GetLength(1); j++)
+      {
+        sumDistSq += Math.Pow(double.CreateChecked(data1[i, j]) - avg1 / avg2 * double.CreateChecked(data2[i, j]), 2);
+      }
+    }
+
+    return Math.Sqrt(sumDistSq / (data1.GetLength(0) * data1.GetLength(1)));
   }
 
   private static Task HandleParseError(IEnumerable<Error> errs)
