b9370f279915e20d4756c03c81008154c20d1945
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-11-29 17:13:32 +0000

    - reSharper guided code cleanup

--- DWTHaar.cs
+++ DWTHaar.cs
@@ -16,17 +16,17 @@ public static class DWTHaar
   /// 
   public static void FWT(double[] data)
   {
-    double[] temp = new double[data.Length];
+    var temp = new double[data.Length];
 
-    int h = data.Length >> 1;
-    for (int i = 0; i < h; i++)
+    var h = data.Length >> 1;
+    for (var i = 0; i < h; i++)
     {
-      int k = (i << 1);
+      var k = (i << 1);
       temp[i] = data[k] * s0 + data[k + 1] * s1;
       temp[i + h] = data[k] * w0 + data[k + 1] * w1;
     }
 
-    for (int i = 0; i < data.Length; i++)
+    for (var i = 0; i < data.Length; i++)
       data[i] = temp[i];
   }
 
@@ -35,41 +35,38 @@ public static class DWTHaar
   /// </summary>
   public static void FWT(double[,] data, int iterations)
   {
-    int rows = data.GetLength(0);
-    int cols = data.GetLength(1);
+    var rows = data.GetLength(0);
+    var cols = data.GetLength(1);
 
-    double[] row;
-    double[] col;
-
-    for (int k = 0; k < iterations; k++)
+    for (var k = 0; k < iterations; k++)
     {
-      int lev = 1 << k;
+      var lev = 1 << k;
 
-      int levCols = cols / lev;
-      int levRows = rows / lev;
+      var levCols = cols / lev;
+      var levRows = rows / lev;
 
-      row = new double[levCols];
-      for (int i = 0; i < levRows; i++)
+      var row = new double[levCols];
+      for (var i = 0; i < levRows; i++)
       {
-        for (int j = 0; j < row.Length; j++)
+        for (var j = 0; j < row.Length; j++)
           row[j] = data[i, j];
 
         FWT(row);
 
-        for (int j = 0; j < row.Length; j++)
+        for (var j = 0; j < row.Length; j++)
           data[i, j] = row[j];
       }
 
 
-      col = new double[levRows];
-      for (int j = 0; j < levCols; j++)
+      var col = new double[levRows];
+      for (var j = 0; j < levCols; j++)
       {
-        for (int i = 0; i < col.Length; i++)
+        for (var i = 0; i < col.Length; i++)
           col[i] = data[i, j];
 
         FWT(col);
 
-        for (int i = 0; i < col.Length; i++)
+        for (var i = 0; i < col.Length; i++)
           data[i, j] = col[i];
       }
     }
@@ -80,17 +77,17 @@ public static class DWTHaar
   /// </summary>
   public static void IWT(double[] data)
   {
-    double[] temp = new double[data.Length];
+    var temp = new double[data.Length];
 
-    int h = data.Length >> 1;
-    for (int i = 0; i < h; i++)
+    var h = data.Length >> 1;
+    for (var i = 0; i < h; i++)
     {
-      int k = (i << 1);
+      var k = (i << 1);
       temp[k] = (data[i] * s0 + data[i + h] * w0) / w0;
       temp[k + 1] = (data[i] * s1 + data[i + h] * w1) / s0;
     }
 
-    for (int i = 0; i < data.Length; i++)
+    for (var i = 0; i < data.Length; i++)
       data[i] = temp[i];
   }
 
@@ -99,40 +96,40 @@ public static class DWTHaar
   /// </summary>
   public static void IWT(double[,] data, int iterations)
   {
-    int rows = data.GetLength(0);
-    int cols = data.GetLength(1);
+    var rows = data.GetLength(0);
+    var cols = data.GetLength(1);
 
     double[] col;
     double[] row;
 
-    for (int k = iterations - 1; k >= 0; k--)
+    for (var k = iterations - 1; k >= 0; k--)
     {
-      int lev = 1 << k;
+      var lev = 1 << k;
 
-      int levCols = cols / lev;
-      int levRows = rows / lev;
+      var levCols = cols / lev;
+      var levRows = rows / lev;
 
       col = new double[levRows];
-      for (int j = 0; j < levCols; j++)
+      for (var j = 0; j < levCols; j++)
       {
-        for (int i = 0; i < col.Length; i++)
+        for (var i = 0; i < col.Length; i++)
           col[i] = data[i, j];
 
         IWT(col);
 
-        for (int i = 0; i < col.Length; i++)
+        for (var i = 0; i < col.Length; i++)
           data[i, j] = col[i];
       }
 
       row = new double[levCols];
-      for (int i = 0; i < levRows; i++)
+      for (var i = 0; i < levRows; i++)
       {
-        for (int j = 0; j < row.Length; j++)
+        for (var j = 0; j < row.Length; j++)
           row[j] = data[i, j];
 
         IWT(row);
 
-        for (int j = 0; j < row.Length; j++)
+        for (var j = 0; j < row.Length; j++)
           data[i, j] = row[j];
       }
     }
