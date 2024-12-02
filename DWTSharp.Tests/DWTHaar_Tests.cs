5a8bfe1bb99e5bf7d6be98736e01e059d41fc9e3
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-12-02 11:10:28 +0000

    - unit tests

--- DWTSharp.Tests/DWTHaar_Tests.cs
+++ DWTSharp.Tests/DWTHaar_Tests.cs
@@ -1,15 +1,23 @@
 ﻿namespace DWTSharp.Tests;
 
-public class DWTHaar_Tests
-{
-  [SetUp]
-  public void Setup()
-  {
-  }
+using FluentAssertions;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
+public sealed class DWTHaar_Tests
+{
   [Test]
-  public void Test1()
+  public void FWT_IWT_succeeds()
   {
-    Assert.Pass();
+    var img = Image.Load<Rgba32>("images/AlysonHannigan_0.jpg");
+    var data = ImageDecomposition.GetGrayscaleDate(img);
+    var copy = (double[,])data.Clone();
+
+    DWTHaar.FWT(data, 1);
+    DWTHaar.IWT(data, 1);
+
+    data.Should().BeEquivalentTo(copy, options => options
+      .Using<double>(ctx => ctx.Subject.Should().BeApproximately(ctx.Expectation, 0.000001d))
+      .WhenTypeIs<double>());
   }
 }
